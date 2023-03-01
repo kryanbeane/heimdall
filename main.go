4e8f43956f14da0bc1d0da2985d1e227d23374eb
Bryan Keane <bkeane@redhat.com>
2023-03-01 12:24:07 +0000

    HEIMCTR-28: Added simple prometheus metric

--- main.go
+++ main.go
@@ -88,7 +88,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// TODO: Need to change this later so that the priority level is configurable (Low, Medium, High)
 	requiredLabelQuery := ".metadata.labels | has(\"app.heimdall.io/watching\")"
 
 	if err = (&controllers.Controller{
