776a56c0a4e72d40edc3edeca84dfb75a750f4f9
Bryan Keane <bkeane@redhat.com>
2023-04-12 10:27:01 +0100

    HEIMCTR-57: Moveod JQ and unstructured logic to helper file

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -3,19 +3,16 @@ package controllers
 import (
 	"bytes"
 	"context"
-	"github.com/gertd/go-pluralize"
 	"github.com/heimdall-controller/heimdall/pkg/controllers/helpers"
 	"github.com/heimdall-controller/heimdall/pkg/slack"
 	"github.com/heimdall-controller/heimdall/pkg/slack/provider"
 	gcp2 "github.com/heimdall-controller/heimdall/pkg/slack/provider/gcp"
-	"github.com/itchyny/gojq"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	u "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
@@ -235,11 +232,6 @@ func (c *Controller) ReconcileNotificationCadence(resource *u.Unstructured, url 
 	return nil
 }
 
-func GVRFromUnstructured(o u.Unstructured) schema.GroupVersionResource {
-	resource := strings.ToLower(pluralize.NewClient().Plural(o.GetObjectKind().GroupVersionKind().Kind))
-	return schema.GroupVersionResource{Group: o.GetObjectKind().GroupVersionKind().Group, Version: o.GetObjectKind().GroupVersionKind().Version, Resource: resource}
-}
-
 func (c *Controller) ReconcileConfigMap(ctx context.Context) (v1.ConfigMap, error) {
 	var cm v1.ConfigMap
 
@@ -321,7 +313,7 @@ func (c *Controller) WatchResources(discoveryClient *discovery.DiscoveryClient, 
 					namespacedName := namespacedName(&item)
 					go func() {
 						// Set up watch for the item
-						watcher, err := dynamicClient.Resource(GVRFromUnstructured(item)).Watch(context.TODO(), metav1.ListOptions{})
+						watcher, err := dynamicClient.Resource(helpers.GVRFromUnstructured(item)).Watch(context.TODO(), metav1.ListOptions{})
 						if err != nil {
 							return
 						}
@@ -365,21 +357,6 @@ func (c *Controller) WatchResources(discoveryClient *discovery.DiscoveryClient, 
 	return nil
 }
 
-func GetUnstructuredResourceList(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string) ([]u.Unstructured, error) {
-	resourceId := schema.GroupVersionResource{
-		Group:    group,
-		Version:  version,
-		Resource: resource,
-	}
-
-	list, err := dynamic.Resource(resourceId).Namespace("").List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	return list.Items, nil
-}
-
 func DiscoverClusterGRVs(ctx context.Context, dc *discovery.DiscoveryClient, di dynamic.Interface, requiredLabelQuery string) ([]u.Unstructured, error) {
 	var g, v, r string
 	var items []u.Unstructured
@@ -411,7 +388,7 @@ func DiscoverClusterGRVs(ctx context.Context, dc *discovery.DiscoveryClient, di 
 					r = resource.Name
 
 					// Get a list of all objects in the group/version/resource in json with the label
-					jqItems, err := GetResourcesByJq(di, ctx, g, v, r, requiredLabelQuery)
+					jqItems, err := helpers.GetResourcesByJq(di, ctx, g, v, r, requiredLabelQuery)
 					if err != nil {
 						continue
 					}
@@ -423,53 +400,4 @@ func DiscoverClusterGRVs(ctx context.Context, dc *discovery.DiscoveryClient, di 
 	}
 
 	return items, nil
-}
-
-func GetResourcesByJq(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string, labelQuery string) ([]u.Unstructured, error) {
-	resources := make([]u.Unstructured, 0)
-
-	query, err := gojq.Parse(labelQuery)
-	if err != nil {
-		logrus.Errorf("error parsing jq query: %v", err)
-		return nil, err
-	}
-
-	items, err := GetUnstructuredResourceList(dynamic, ctx, group, version, resource)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, item := range items {
-
-		// Convert object to raw JSON
-		var rawJson interface{}
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, &rawJson)
-		if err != nil {
-			logrus.Errorf("error converting object to raw JSON: %v", err)
-			return nil, err
-		}
-
-		// Evaluate jq against JSON
-		iter := query.Run(rawJson)
-		for {
-			result, ok := iter.Next()
-			if !ok {
-				break
-			}
-			if err, ok := result.(error); ok {
-				if err != nil {
-					logrus.Errorf("error evaluating jq: %v", err)
-					return nil, err
-				}
-			} else {
-				boolResult, ok := result.(bool)
-				if !ok {
-					logrus.Errorf("error converting jq result to bool: %v", err)
-				} else if boolResult {
-					resources = append(resources, item)
-				}
-			}
-		}
-	}
-	return resources, nil
 }
---
+++ pkg/controllers/helpers/jq.go
@@ -0,0 +1,83 @@
+package helpers
+
+import (
+	"context"
+	"github.com/gertd/go-pluralize"
+	"github.com/itchyny/gojq"
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	u "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"strings"
+)
+
+func GetResourcesByJq(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string, labelQuery string) ([]u.Unstructured, error) {
+	resources := make([]u.Unstructured, 0)
+
+	query, err := gojq.Parse(labelQuery)
+	if err != nil {
+		logrus.Errorf("error parsing jq query: %v", err)
+		return nil, err
+	}
+
+	items, err := GetUnstructuredResourceList(dynamic, ctx, group, version, resource)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range items {
+
+		// Convert object to raw JSON
+		var rawJson interface{}
+		err = runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, &rawJson)
+		if err != nil {
+			logrus.Errorf("error converting object to raw JSON: %v", err)
+			return nil, err
+		}
+
+		// Evaluate jq against JSON
+		iter := query.Run(rawJson)
+		for {
+			result, ok := iter.Next()
+			if !ok {
+				break
+			}
+			if err, ok := result.(error); ok {
+				if err != nil {
+					logrus.Errorf("error evaluating jq: %v", err)
+					return nil, err
+				}
+			} else {
+				boolResult, ok := result.(bool)
+				if !ok {
+					logrus.Errorf("error converting jq result to bool: %v", err)
+				} else if boolResult {
+					resources = append(resources, item)
+				}
+			}
+		}
+	}
+	return resources, nil
+}
+
+func GetUnstructuredResourceList(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string) ([]u.Unstructured, error) {
+	resourceId := schema.GroupVersionResource{
+		Group:    group,
+		Version:  version,
+		Resource: resource,
+	}
+
+	list, err := dynamic.Resource(resourceId).Namespace("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return list.Items, nil
+}
+
+func GVRFromUnstructured(o u.Unstructured) schema.GroupVersionResource {
+	resource := strings.ToLower(pluralize.NewClient().Plural(o.GetObjectKind().GroupVersionKind().Kind))
+	return schema.GroupVersionResource{Group: o.GetObjectKind().GroupVersionKind().Group, Version: o.GetObjectKind().GroupVersionKind().Version, Resource: resource}
+}
