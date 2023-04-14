8b2af76021c4804133f2504a6a8fea800f83dde3
Bryan Keane <bkeane@redhat.com>
2023-04-14 10:46:42 +0100

    HEIMCTR-48: Added script to install kafka via strimzi operator

--- template/scripts/strimzi.sh
+++ template/scripts/strimzi.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-kubectl create -f 'https://strimzi.io/install/latest?namespace=heimdall' -n heimdall
+NAMESPACE="heimdall"
+KAFKA_YAML="./template/kafka.yaml"
 
-kubectl apply -f ./template/kafka.yaml -n heimdall
+echo "Creating Strimzi Operator and Kafka Cluster in namespace $NAMESPACE..."
+if ! kubectl create -f "https://strimzi.io/install/latest?namespace=$NAMESPACE" -n "$NAMESPACE" 2>&1 | grep -q "already exists"; then
+    echo "Error creating Strimzi Operator" >&2
+    exit 1
+fi
 
-kubectl wait heimdall/heimdall-cluster --for=condition=Ready --timeout=300s -n kafka
+if ! kubectl apply -f "$KAFKA_YAML" -n "$NAMESPACE"; then
+    echo "Error creating Kafka Cluster" >&2
+    exit 1
+fi
 
+echo "Waiting for Kafka Cluster to be ready..."
+if ! kubectl wait kafka/heimdall-kafka-cluster --for=condition=Ready --timeout=300s -n "$NAMESPACE"; then
+    echo "Error waiting for Kafka Cluster to be ready" >&2
+    exit 1
+fi
+
+echo "Strimzi Operator and Kafka Cluster initialized in namespace $NAMESPACE"
--- template/scripts/uninstall.sh
+++ template/scripts/uninstall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "Uninstalling Strimzi Operator and Kafka Cluster..."
+
 kubectl delete -f 'https://strimzi.io/install/latest?namespace=heimdall' -n heimdall
+
+echo "Uninstalling admission controller and webhook..."
 
 kubectl delete -A MutatingWebhookConfiguration heimdall-webhook
 
-kubectl delete Deployment heimdall -n heimdall
-
 kubectl delete Deployment heimdall-admission-controller -n heimdall
 
+echo "Finalizing Heimdall's uninstall..."
+
+kubectl delete Deployment heimdall -n heimdall
+
 kubectl delete ns heimdall
+
+echo "Heimdall uninstalled, we'll miss you :("
