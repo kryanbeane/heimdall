dcf6b4f99a9107199a80023b58c6a04e69c295b7
Bryan Keane <bkeane@redhat.com>
2023-04-14 17:57:49 +0100

    HEIMCTR-48: Updated uninstall script to completely remove admission controller

--- template/scripts/uninstall.sh
+++ template/scripts/uninstall.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+set -e
+
+# variables
+NAMESPACE="heimdall"
+WEBHOOK="heimdall-webhook"
+ROLE_BINDING="admission-role-binding"
+ROLE="admission-role"
+CONTROLLER="heimdall-admission-controller"
+TLS_SECRET="heimdall-admission-controller-tls"
+SERVICE="heimdall-admission-controller"
+DEPLOYMENT="heimdall"
+
 echo "Uninstalling Strimzi Operator and Kafka Cluster..."
+kubectl delete -f 'https://strimzi.io/install/latest?namespace=heimdall' -n $NAMESPACE
 
-kubectl delete -f 'https://strimzi.io/install/latest?namespace=heimdall' -n heimdall
+echo "Uninstalling Admission Controller..."
+kubectl delete -A MutatingWebhookConfiguration $WEBHOOK
+kubectl delete RoleBinding $ROLE_BINDING -n $NAMESPACE
+kubectl delete Role $ROLE -n $NAMESPACE
+kubectl delete Deployment $CONTROLLER -n $NAMESPACE
+kubectl delete Secret $TLS_SECRET -n $NAMESPACE
+kubectl delete Service $SERVICE -n $NAMESPACE
 
-echo "Uninstalling admission controller and webhook..."
+echo "Finalizing Heimdall's removal..."
 
-kubectl delete -A MutatingWebhookConfiguration heimdall-webhook
-
-kubectl delete Deployment heimdall-admission-controller -n heimdall
-
-echo "Finalizing Heimdall's uninstall..."
-
-kubectl delete Deployment heimdall -n heimdall
-
-kubectl delete ns heimdall
+kubectl delete Deployment $DEPLOYMENT -n $NAMESPACE
+kubectl delete ns $NAMESPACE
 
 echo "Heimdall uninstalled, we'll miss you :("
